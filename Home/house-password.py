3cccdebddabca13f695f2e1fa6ea961a9ad3eb16
anbreaker <antunez19@gmail.com>
2019-12-26 17:11:54 +0100

    better conditions var

--- Home/house-password.py
+++ Home/house-password.py
@@ -1,47 +1,17 @@
-# Stephan and Sophia forget about security and use simple passwords for everything.
-# Help Nikola develop a password security check module. The password will be considered
-# strong enough if:
-#    its length is greater than or equal to 10 symbols,
-#    it has at least one digit,
-#    as well as containing one uppercase letter and
-#    one lowercase letter in it.
-#    The password contains only ASCII latin letters or digits.
-
-# Input: A password as a string.
-
-# Output: Is the password safe or not as a boolean or any data type that can be
-# converted and processed as a boolean. In the results you will see the converted results.
-
-# Example:
-# checkio('A1213pokl') == False
-# checkio('bAse730onE') == True
-# checkio('asasasasasasasaas') == False
-# checkio('QWERTYqwerty') == False
-# checkio('123456123456') == False
-# checkio('QwErTy911poqqqq') == True
-
-# Precondition:
-# re.match("[a-zA-Z0-9]+", password)
-# 0 < len(password) ≤ 64
-
-
 def checkio(data: str) -> bool:
-    oneCondition = 0
-    secondContion = 0
-    thirdCondition = 0
-    fourthCondition = 0
+    oneNumber = 0
+    oneUpperCase = 0
+    oneLowerCase = 0
     if data.isalnum() and len(data) >= 10:  # The password contains only ASCII latin letters or digits
         for d in data:                      # Greater than or equal to 10 symbols
-            if d.isdigit():
-                oneCondition = 1
-            if d.isupper() == True:  # as well as containing one uppercase letter
-                secondContion = 1
-            if d.islower() == True:  # one lowercase letter in it
-                thirdCondition = 1
-            if d.isdigit() == True:  # # it has at least one digit
-                fourthCondition = 1
+            if d.isdigit():                 # it has at least one digit
+                oneNumber = 1
+            if d.isupper() == True:         # as well as containing one uppercase letter
+                oneUpperCase = 1
+            if d.islower() == True:         # one lowercase letter in it
+                oneLowerCase = 1
 
-        if oneCondition == 1 and secondContion == 1 and thirdCondition == 1 and fourthCondition == 1:
+        if oneNumber == 1 and oneUpperCase == 1 and oneLowerCase == 1:
             return True
     return False
 
